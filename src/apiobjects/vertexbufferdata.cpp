f037e373e6423a397c35d629e83b1269cf30632f
Sergey Reznik <rezniksa@gmail.com>
2014-03-22 23:30:42 +0200

    vertex buffer data - new setData method - performance improved

--- src/apiobjects/vertexbufferdata.cpp
+++ src/apiobjects/vertexbufferdata.cpp
@@ -12,7 +12,7 @@ using namespace et;
 
 VertexBufferData::VertexBufferData(RenderContext* rc, const VertexArray::Description& desc,
 	BufferDrawType vertexDrawType, const std::string& aName) : Object(aName), _rc(rc), _vertexBuffer(0),
-	_decl(desc.declaration), _dataSize(desc.data.dataSize()), _sourceTag(0), _drawType(vertexDrawType)
+	_decl(desc.declaration), _dataSize(0), _sourceTag(0), _drawType(vertexDrawType)
 {
 	glGenBuffers(1, &_vertexBuffer);
 	setData(desc.data.data(), desc.data.dataSize());
@@ -20,7 +20,7 @@ VertexBufferData::VertexBufferData(RenderContext* rc, const VertexArray::Descrip
 
 VertexBufferData::VertexBufferData(RenderContext* rc, const VertexDeclaration& decl, const void* vertexData,
 	size_t vertexDataSize, BufferDrawType vertexDrawType, const std::string& aName) : Object(aName), _rc(rc),
-	_vertexBuffer(0), _decl(decl), _dataSize(vertexDataSize), _sourceTag(0), _drawType(vertexDrawType)
+	_vertexBuffer(0), _decl(decl), _dataSize(0), _sourceTag(0), _drawType(vertexDrawType)
 {
 	glGenBuffers(1, &_vertexBuffer);
 	setData(vertexData, vertexDataSize);
@@ -36,14 +36,15 @@ VertexBufferData::~VertexBufferData()
 
 void VertexBufferData::setData(const void* data, size_t dataSize)
 {
+	_rc->renderState().bindBuffer(GL_ARRAY_BUFFER, _vertexBuffer);
+	
+	glBufferData(GL_ARRAY_BUFFER, static_cast<GLsizeiptr>(_dataSize), nullptr, drawTypeValue(_drawType));
+	checkOpenGLError("glBufferData(GL_ARRAY_BUFFER, %u, 0x%08X, %d)", _dataSize, data, _drawType);
+	
 	_dataSize = dataSize;
 	
-	if (_dataSize > 0)
-	{
-		_rc->renderState().bindBuffer(GL_ARRAY_BUFFER, _vertexBuffer);
-		glBufferData(GL_ARRAY_BUFFER, static_cast<GLsizeiptr>(_dataSize), data, drawTypeValue(_drawType));
-		checkOpenGLError("glBufferData(GL_ARRAY_BUFFER, %u, 0x%08X, ..,)", _dataSize, data);
-	}
+	glBufferData(GL_ARRAY_BUFFER, static_cast<GLsizeiptr>(_dataSize), data, drawTypeValue(_drawType));
+	checkOpenGLError("glBufferData(GL_ARRAY_BUFFER, %u, 0x%08X, %d)", _dataSize, data, _drawType);
 }
 
 void VertexBufferData::serialize(std::ostream&)
