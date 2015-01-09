25bcf266c5b5f4d76c9bc341f70837d6c55ce958
Sergey Reznik <rezniksa@gmail.com>
2015-01-09 14:07:17 +0200

    allowed file types for selectFile function in OS X

--- src/platform-mac/platformtools.mac.mm
+++ src/platform-mac/platformtools.mac.mm
@@ -16,20 +16,23 @@ using namespace et;
 
 typedef void (^filePickerCallback)(__strong NSString* path);
 
-@interface FilePicker : NSObject
+static NSString* rootFolder = @"/";
+
+@interface FilePicker : NSObject<NSOpenSavePanelDelegate>
 {
 	NSString* _defaultName;
+	NSArray* _allowedExtensions;
 	filePickerCallback _callback;
 }
 
-- (instancetype)initWithDefaultName:(NSString*)name callback:(filePickerCallback)cb;
+- (instancetype)initWithDefaultName:(NSString*)name fileTypes:(NSArray*)fileTypes callback:(filePickerCallback)cb;
 
 - (void)openFile;
 - (void)saveFile;
 
 @end
 
-std::string et::selectFile(const StringList&, SelectFileMode mode, const std::string& defaultName)
+std::string et::selectFile(const StringList& allowedTypes, SelectFileMode mode, const std::string& defName)
 {
 	SEL selector = nil;
 	
@@ -42,8 +45,14 @@ std::string et::selectFile(const StringList&, SelectFileMode mode, const std::st
 	
 	__block std::string result;
 	
-	FilePicker* picker = ET_OBJC_AUTORELEASE([[FilePicker alloc]
-		initWithDefaultName:[NSString stringWithUTF8String:defaultName.c_str()] callback:^(__strong NSString* path)
+	NSString* defaultName = [NSString stringWithUTF8String:defName.c_str()];
+	
+	NSMutableArray* fileTypes = [NSMutableArray array];
+	for (const auto& ext : allowedTypes)
+		[fileTypes addObject:[[NSString stringWithUTF8String:ext.c_str()] lowercaseString]];
+	
+	FilePicker* picker = ET_OBJC_AUTORELEASE([[FilePicker alloc] initWithDefaultName:defaultName
+		fileTypes:fileTypes callback:^(__strong NSString* path)
 	{
 		result = std::string([path UTF8String]);
 	}]);
@@ -54,40 +63,70 @@ std::string et::selectFile(const StringList&, SelectFileMode mode, const std::st
 
 @implementation FilePicker
 
-- (instancetype)initWithDefaultName:(NSString*)name callback:(filePickerCallback)cb
+- (instancetype)initWithDefaultName:(NSString*)name fileTypes:(NSArray *)fileTypes callback:(filePickerCallback)cb
 {
 	self = [super init];
 	if (self)
 	{
+		_allowedExtensions = fileTypes;
 		_defaultName = name;
 		_callback = cb;
 	}
 	return self;
 }
 
+- (BOOL)panel:(id)sender shouldEnableURL:(NSURL*)url
+{
+	NSError* error = nil;
+	NSDictionary* resourceValues = [url resourceValuesForKeys:@[NSURLIsRegularFileKey, NSURLIsDirectoryKey] error:&error];
+	
+	if ([[resourceValues objectForKey:NSURLIsDirectoryKey] boolValue])
+		return YES;
+
+	if ([_allowedExtensions count] == 0)
+		return YES;
+	
+	if ([[resourceValues objectForKey:NSURLIsRegularFileKey] boolValue])
+	{
+		NSString* fileExtension = [[url pathExtension] lowercaseString];
+		return [_allowedExtensions containsObject:fileExtension];
+	}
+
+	return NO;
+}
+
 - (void)openFile
 {
 #if defined(ET_CONSOLE_APPLICATION)
+	
 	_callback([NSString string]);
+	
 #else
+	
 	NSOpenPanel* openDlg = [NSOpenPanel openPanel];
+	[openDlg setDelegate:self];
 	[openDlg setCanChooseFiles:YES];
 	[openDlg setCanChooseDirectories:NO];
 	[openDlg setAllowsMultipleSelection:NO];
 	[openDlg setResolvesAliases:YES];
-	[openDlg setAllowedFileTypes:nil];
+	
 	_callback([openDlg runModal] == NSOKButton ? [[[openDlg URLs] objectAtIndex:0] path] : [NSString string]);
+	
 #endif
 }
 
 - (void)saveFile
 {
 #if defined(ET_CONSOLE_APPLICATION)
+	
 	_callback([NSString string]);
+	
 #else
+	
 	NSSavePanel* saveDlg = [NSSavePanel savePanel];
 	[saveDlg setNameFieldStringValue:_defaultName];
 	_callback([saveDlg runModal] == NSOKButton ? [[saveDlg URL] path] : [NSString string]);
+	
 #endif
 }
 
