a5be98c09cf9a4fdc012cc320129f22019ee05c5
Sergey Reznik <rezniksa@gmail.com>
2015-10-18 13:15:38 +0300

    et_key_h added for windows

--- include/et/core/constants.h
+++ include/et/core/constants.h
@@ -102,6 +102,7 @@
 #	define ET_KEY_S						'S'	
 #	define ET_KEY_D						'D'	
 #	define ET_KEY_W						'W'	
+#	define ET_KEY_H						'H'	
 #	define ET_KEY_1						49
 #	define ET_KEY_2						50
 #	define ET_KEY_3						51
