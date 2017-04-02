2a4f6ee03c8b86f76f7a6d5024c50b63815c318b
Sergey Reznik <rezniksa@gmail.com>
2017-04-02 13:34:18 +0300

    Fast scattering for objects

--- engine_data/materials/shaders/atmosphere.h
+++ engine_data/materials/shaders/atmosphere.h
@@ -13,85 +13,106 @@
 
 float atmosphereIntersection(in float3 origin, in float3 direction)
 {
-	float b = dot(direction, origin);
-	float d = b * b - dot(origin, origin) + Ra * Ra;
-	return (d < 0.0) ? -1.0 : (-b + sqrt(d));
+    float b = dot(direction, origin);
+    float d = b * b - dot(origin, origin) + Ra * Ra;
+    return (d < 0.0) ? -1.0 : (-b + sqrt(d));
 }
 
 float planetIntersection(in float3 origin, in float3 direction)
 {
-	float b = dot(direction, origin);
-	float d = b * b - dot(origin, origin) + Re * Re;
-	return (d < 0.0) ? -1.0 : (-b - sqrt(d));
+    float b = dot(direction, origin);
+    float d = b * b - dot(origin, origin) + Re * Re;
+    return (d < 0.0) ? -1.0 : (-b - sqrt(d));
 }
 
 float phaseFunctionRayleigh(in float cosTheta)
 {
-	return (3.0 / 4.0) * (1.0 + cosTheta * cosTheta) / (4.0 * PI);
+    return (3.0 / 4.0) * (1.0 + cosTheta * cosTheta) / (4.0 * PI);
 }
 
 float phaseFunctionMie(in float cosTheta, in float g)
 {
-	return (3.0 * (1.0 - g * g)) / (2.0 * (2.0 + g * g) * 
-		(1.0 + cosTheta * cosTheta) ) / pow(abs(1.0 + g * g - 2.0 * g * cosTheta), 3.0 / 2.0) / (4.0 * PI);
+    return (3.0 * (1.0 - g * g)) / (2.0 * (2.0 + g * g) * 
+        (1.0 + cosTheta * cosTheta) ) / pow(abs(1.0 + g * g - 2.0 * g * cosTheta), 3.0 / 2.0) / (4.0 * PI);
 }
 
 float2 density(in float3 pos)
 {
-	float h = max(0.0, length(pos) - Re);
-	return float2(exp(-h / H0r), exp(-h / H0m));
+    float h = max(0.0, length(pos) - Re);
+    return float2(exp(-h / H0r), exp(-h / H0m));
 }
 
 float2 opticalDensity(in float3 origin, in float3 dir)
 {
-	float3 dp = dir / outScatteringSamples;
-	origin += 0.5 * dp;
-	
-	float2 result = 0.0;
-	for (uint i = 0; i < outScatteringSamples; ++i)
-	{
-		result += density(origin);
-		origin += dp;
-	}
-	return result * length(dp);
+    float3 dp = dir / outScatteringSamples;
+    origin += 0.5 * dp;
+    
+    float2 result = 0.0;
+    for (uint i = 0; i < outScatteringSamples; ++i)
+    {
+        result += density(origin);
+        origin += dp;
+    }
+    return result * length(dp);
 }
 
 float3 inScattering(in float3 origin, in float3 target, in float3 light, in float2 phase)
 {
-	float3 step = (target - origin) / inScatteringSamples;
+    float3 step = (target - origin) / inScatteringSamples;
 
-	float3 resultR = 0.0;
-	float3 resultM = 0.0;
-	float3 pos = origin + 0.5 * step;
-	float2 totalDensity = 0.0;
-	for (uint i = 0; i < inScatteringSamples; ++i)
-	{
-		float2 d = density(pos);
+    float3 resultR = 0.0;
+    float3 resultM = 0.0;
+    float3 pos = origin + 0.5 * step;
+    float2 totalDensity = 0.0;
+    for (uint i = 0; i < inScatteringSamples; ++i)
+    {
+        float2 d = density(pos);
 
-		float3 toLight = light * atmosphereIntersection(pos, light);
-		float2 opticalDepth = opticalDensity(pos, toLight) + opticalDensity(pos, origin - pos);
-		float3 scattering = exp(-betaR * (totalDensity.x + opticalDepth.x) - betaM * (totalDensity.y + opticalDepth.y));
+        float3 toLight = light * atmosphereIntersection(pos, light);
+        float2 opticalDepth = opticalDensity(pos, toLight) + opticalDensity(pos, origin - pos);
+        float3 scattering = exp(-betaR * (totalDensity.x + opticalDepth.x) - betaM * (totalDensity.y + opticalDepth.y));
 
-		resultR += d.x * scattering;
-		resultM += d.y * scattering;
+        resultR += d.x * scattering;
+        resultM += d.y * scattering;
 
-		totalDensity += d;
-		pos += step;
-	}
-	return (resultR * betaR * phase.x + resultM * betaM * phase.y) * length(step);
+        totalDensity += d;
+        pos += step;
+    }
+    return (resultR * betaR * phase.x + resultM * betaM * phase.y) * length(step);
 }
 
 float3 outScattering(in float3 origin, in float3 target)
 {
-	float2 scatter = opticalDensity(origin, target - origin);
-	return exp(-(scatter.x * betaR + scatter.y * betaM));	
+    float2 scatter = opticalDensity(origin, target - origin);
+    return exp(-(scatter.x * betaR + scatter.y * betaM));   
+}
+
+float2 opticalDensityAtConstantHeight(in float3 origin, in float3 dir)
+{
+    return density(origin + 0.5 * dir) * length(dir);
+}
+
+float3 inScatteringAtConstantHeight(in float3 origin, in float3 target, in float3 light, in float2 phase)
+{
+    float2 constantDensity = density(origin);
+    float2 opticalDepthToLight = opticalDensity(origin, light * atmosphereIntersection(origin, light));
+    float2 opticalDepthToOrigin = opticalDensity(target, origin - target);
+    float2 opticalDepth = opticalDepthToLight + opticalDepthToOrigin;
+    float3 result = exp(-betaR * opticalDepth.x - betaM * opticalDepth.y);
+    return result * (betaR * constantDensity.x * phase.x + constantDensity.y * betaM * phase.y) * length(target - origin);
+}
+
+float3 outScatteringAtConstantHeight(in float3 origin, in float3 target)
+{
+    float2 scatter = density(target) * length(target - origin);
+    return exp(-betaR * scatter.x - betaM * scatter.y);
 }
 
 float3 sampleAtmosphere(float3 dir, in float3 light, in float3 lightColor)
 {
-	float cosTheta = dot(dir, light);
-	float2 phase = float2(phaseFunctionRayleigh(cosTheta), phaseFunctionMie(cosTheta, mieG));
+    float cosTheta = dot(dir, light);
+    float2 phase = float2(phaseFunctionRayleigh(cosTheta), phaseFunctionMie(cosTheta, mieG));
 
-	float a = atmosphereIntersection(positionOnPlanet, dir);
-	return lightColor * inScattering(positionOnPlanet, positionOnPlanet + a * dir, light, phase);
+    float a = atmosphereIntersection(positionOnPlanet, dir);
+    return lightColor * inScattering(positionOnPlanet, positionOnPlanet + a * dir, light, phase);
 }
--- engine_data/materials/shaders/microfacet.hlsl
+++ engine_data/materials/shaders/microfacet.hlsl
@@ -4,22 +4,22 @@
 
 cbuffer MaterialVariables : DECL_BUFFER(Material)
 {
-	float4 diffuseReflectance;
-	float4 specularReflectance;
-	float4 emissiveColor;
-	float roughnessScale;
-	float metallnessScale;
+    float4 diffuseReflectance;
+    float4 specularReflectance;
+    float4 emissiveColor;
+    float roughnessScale;
+    float metallnessScale;
 };
 
 cbuffer ObjectVariables : DECL_BUFFER(Object)
 {
-	row_major float4x4 viewProjectionTransform;
-	row_major float4x4 worldTransform;
-	row_major float4x4 worldRotationTransform;
-	row_major float4x4 lightProjectionTransform;
-	float3 lightColor;
-	float4 lightDirection;
-	float4 cameraPosition;
+    row_major float4x4 viewProjectionTransform;
+    row_major float4x4 worldTransform;
+    row_major float4x4 worldRotationTransform;
+    row_major float4x4 lightProjectionTransform;
+    float3 lightColor;
+    float4 lightDirection;
+    float4 cameraPosition;
 };
 
 Texture2D<float4> baseColorTexture : DECL_TEXTURE(BaseColor);
@@ -39,36 +39,36 @@ SamplerState opacitySampler : DECL_SAMPLER(Opacity);
 
 struct VSOutput 
 {
-	float4 position : SV_Position;
-	float3 normal;
-	float3 toLight;
-	float3 toCamera;
-	float2 texCoord0;
-	float4 lightCoord;
-	float3 invTransformT;
-	float3 invTransformB;
-	float3 invTransformN;
+    float4 position : SV_Position;
+    float3 normal;
+    float3 toLight;
+    float3 toCamera;
+    float2 texCoord0;
+    float4 lightCoord;
+    float3 invTransformT;
+    float3 invTransformB;
+    float3 invTransformN;
 };
 
 VSOutput vertexMain(VSInput vsIn)
 {
-	float4 transformedPosition = mul(float4(20.0 * vsIn.position, 1.0), worldTransform);
-	
-	VSOutput vsOut;
-	vsOut.texCoord0 = vsIn.texCoord0;
-	vsOut.normal = normalize(mul(float4(vsIn.normal, 0.0), worldRotationTransform).xyz);
+    float4 transformedPosition = mul(float4(10.0 * vsIn.position, 1.0), worldTransform);
+    
+    VSOutput vsOut;
+    vsOut.texCoord0 = vsIn.texCoord0;
+    vsOut.normal = normalize(mul(float4(vsIn.normal, 0.0), worldRotationTransform).xyz);
 
-	float3 tTangent = normalize(mul(float4(vsIn.tangent, 0.0), worldRotationTransform).xyz);
-	float3 tBiTangent = cross(vsOut.normal, tTangent);
+    float3 tTangent = normalize(mul(float4(vsIn.tangent, 0.0), worldRotationTransform).xyz);
+    float3 tBiTangent = cross(vsOut.normal, tTangent);
 
-	vsOut.toCamera = (cameraPosition.xyz - transformedPosition.xyz).xyz;
-	vsOut.toLight = (lightDirection.xyz - transformedPosition.xyz * lightDirection.w).xyz;
-	vsOut.lightCoord = mul(transformedPosition, lightProjectionTransform);
-	vsOut.position = mul(transformedPosition, viewProjectionTransform);
-	vsOut.invTransformT = float3(tTangent.x, tBiTangent.x, vsOut.normal.x);
-	vsOut.invTransformB = float3(tTangent.y, tBiTangent.y, vsOut.normal.y);
-	vsOut.invTransformN = float3(tTangent.z, tBiTangent.z, vsOut.normal.z);
-	return vsOut;
+    vsOut.toCamera = (cameraPosition.xyz - transformedPosition.xyz).xyz;
+    vsOut.toLight = (lightDirection.xyz - transformedPosition.xyz * lightDirection.w).xyz;
+    vsOut.lightCoord = mul(transformedPosition, lightProjectionTransform);
+    vsOut.position = mul(transformedPosition, viewProjectionTransform);
+    vsOut.invTransformT = float3(tTangent.x, tBiTangent.x, vsOut.normal.x);
+    vsOut.invTransformB = float3(tTangent.y, tBiTangent.y, vsOut.normal.y);
+    vsOut.invTransformN = float3(tTangent.z, tBiTangent.z, vsOut.normal.z);
+    return vsOut;
 }
 
 #include "srgb.h"
@@ -79,61 +79,52 @@ VSOutput vertexMain(VSInput vsIn)
 
 float4 fragmentMain(VSOutput fsIn) : SV_Target0
 {
-	float4 normalSample = normalTexture.Sample(normalSampler, fsIn.texCoord0);
-	float4 baseColorSample = baseColorTexture.Sample(baseColorSampler, fsIn.texCoord0);
-	float4 opacitySample = opacityTexture.Sample(opacitySampler, fsIn.texCoord0);
+    float4 normalSample = normalTexture.Sample(normalSampler, fsIn.texCoord0);
+    float4 baseColorSample = baseColorTexture.Sample(baseColorSampler, fsIn.texCoord0);
+    float4 opacitySample = opacityTexture.Sample(opacitySampler, fsIn.texCoord0);
 
-	/*
-	baseColorSample.xyz = float3(1, 1, 1) * 0.18;
-	baseColorSample.w = 1.0;
-	normalSample.xyz = float3(0.5, 0.5, 1);
-	normalSample.w = 0.0;
-	// */
+    if ((opacitySample.x + opacitySample.y) < 127.0 / 255.0) 
+        discard;
 
-	if ((opacitySample.x + opacitySample.y) < 127.0 / 255.0) 
-		discard;
+    float3 baseColor = srgbToLinear(baseColorSample.xyz);
+    Surface surface = buildSurface(baseColor, normalSample.w, baseColorSample.w);
 
-	float3 baseColor = srgbToLinear(baseColorSample.xyz);
-	Surface surface = buildSurface(baseColor, normalSample.w, baseColorSample.w);
+    float3 tsNormal = normalize(normalSample.xyz - 0.5);
 
-	float3 tsNormal = normalize(normalSample.xyz - 0.5);
+    float3 wsNormal;
+    wsNormal.x = dot(fsIn.invTransformT, tsNormal);
+    wsNormal.y = dot(fsIn.invTransformB, tsNormal);
+    wsNormal.z = dot(fsIn.invTransformN, tsNormal);
+    wsNormal = normalize(wsNormal);
 
-	float3 wsNormal;
-	wsNormal.x = dot(fsIn.invTransformT, tsNormal);
-	wsNormal.y = dot(fsIn.invTransformB, tsNormal);
-	wsNormal.z = dot(fsIn.invTransformN, tsNormal);
-	wsNormal = normalize(wsNormal);
+    float3 wsLight = normalize(fsIn.toLight);
+    float3 wsView = normalize(fsIn.toCamera);
 
-	float3 wsLight = normalize(fsIn.toLight);
-	float3 wsView = normalize(fsIn.toCamera);
+    BSDF bsdf = buildBSDF(wsNormal, wsLight, wsView);
+    float3 directDiffuse = computeDirectDiffuse(surface, bsdf);
+    float3 directSpecular = computeDirectSpecular(surface, bsdf);
 
-	BSDF bsdf = buildBSDF(wsNormal, wsLight, wsView);
-	float3 directDiffuse = computeDirectDiffuse(surface, bsdf);
-	float3 directSpecular = computeDirectSpecular(surface, bsdf);
+    float4 brdfLookupSample = brdfLookupTexture.Sample(brdfLookupSampler, float2(surface.roughness, bsdf.VdotN));
 
-	float4 brdfLookupSample = brdfLookupTexture.Sample(brdfLookupSampler, float2(surface.roughness, bsdf.VdotN));
+    float3 wsDiffuseDir = diffuseDominantDirection(wsNormal, wsView, surface.roughness);
+    float3 indirectDiffuse = (surface.baseColor * sampleEnvironment(wsDiffuseDir, lightDirection.xyz, 8.0)) *
+        ((1.0 - surface.metallness) * brdfLookupSample.z);
+                                                                  
+    float3 wsSpecularDir = specularDominantDirection(wsNormal, wsView, surface.roughness);
+    float3 indirectSpecular = sampleEnvironment(wsSpecularDir, lightDirection.xyz, 8.0 * surface.roughness);
+    indirectSpecular *= (surface.f0 * brdfLookupSample.x + surface.f90 * brdfLookupSample.y);
 
-	float3 wsDiffuseDir = diffuseDominantDirection(wsNormal, wsView, surface.roughness);
-	float3 indirectDiffuse = (surface.baseColor * sampleEnvironment(wsDiffuseDir, lightDirection.xyz, 8.0)) *
-		((1.0 - surface.metallness) * brdfLookupSample.z);
-	                                                              
-	float3 wsSpecularDir = specularDominantDirection(wsNormal, wsView, surface.roughness);
-	float3 indirectSpecular = sampleEnvironment(wsSpecularDir, lightDirection.xyz, 8.0 * surface.roughness);
-	indirectSpecular *= (surface.f0 * brdfLookupSample.x + surface.f90 * brdfLookupSample.y);
+    float3 result = lightColor * (directDiffuse + directSpecular) + (indirectDiffuse + indirectSpecular); 
 
-	float3 result = lightColor * (directDiffuse + directSpecular) + (indirectDiffuse + indirectSpecular); 
+    float3 originPosition = positionOnPlanet + cameraPosition.xyz;
+    float3 worldPosition = originPosition - fsIn.toCamera;
+    float3 outScatter = outScatteringAtConstantHeight(originPosition, worldPosition);
 
-	/*
-	float3 originPosition = positionOnPlanet + cameraPosition.xyz;
-	float3 worldPosition = originPosition - 100.0 * fsIn.toCamera;
-	float3 outScatter = outScattering(originPosition, worldPosition);
+    float phaseR = phaseFunctionRayleigh(dot(wsView, wsLight));
+    float phaseM = phaseFunctionMie(dot(wsView, wsLight), mieG);
+    float3 inScatter = lightColor * inScatteringAtConstantHeight(originPosition, worldPosition, wsLight, float2(phaseR, phaseM));
 
-	float cosTheta = -dot(wsView, wsLight);
-	float2 phase = float2(phaseFunctionRayleigh(cosTheta), phaseFunctionMie(cosTheta, mieG));
-	float3 inScatter = lightColor * inScattering(originPosition, worldPosition, wsLight, float2(phase.x, 0.0));
-
-	result = result * outScatter + inScatter;
-	// */                 
-	
-	return float4(result, 1.0);
+    result = result * outScatter + inScatter;
+    
+    return float4(result, 1.0);
 }                              
