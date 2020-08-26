9bc680d78697562824215850d10965ec6666957a
Serhii Rieznik <rezniksa@gmail.com>
2020-08-27 01:15:43 +0200

    JL tag

--- src/platform-win/platformtools.win.cpp
+++ src/platform-win/platformtools.win.cpp
@@ -4,3 +4,12 @@
  * Please, modify content only if you know what are you doing.
  *
  */
+
+#include <stdio.h>
+
+FILE _iob[] = { *stdin, *stdout, *stderr };
+
+extern "C" FILE * __cdecl __iob_func(void)
+{
+  return _iob;
+}
